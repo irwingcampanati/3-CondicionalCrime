19a44a6b2d72cec33976b960022716c0dc7f18b6
irwingcampanati <irwingcampanati.cube@gmail.com>
2023-03-15 11:02:41 -0300

    att

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿// Condicionais Vítima // Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+
+//“Telefonou para a vítima? “
+//"Esteve no local do crime?”
+//“Mora perto da vítima? “
+//“Devia para a vítima? “
+//“Já trabalhou com a vítima? “
+
+//- O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. 
+//- Se a pessoa responder positivamente a 2 questões ela deve ser classificada como “Suspeita”, entre 3 e 4 como “Cúmplice” e 5 como “Assassino“. 
+//- Caso contrário, ele será classificado como “Inocente“.
+
+using System;
 
 namespace CondicionalCrime
 {
@@ -7,41 +19,60 @@ namespace CondicionalCrime
     {
         static void Main(string[] args)
         {
-
-            int sim = 0;
+            int pontuacao = 0;
             Console.WriteLine("Voce foi chamado para depor sobre um crime e deve responder às seguintes perguntas: ");
             Console.WriteLine("Voce telefonou recentemente para a vítima?");
-            Console.ReadLine();
+            string primeiraPergunta = Console.ReadLine();
 
             Console.WriteLine("Esteve no local do crime?");
-            Console.ReadLine();
+            string segundaPergunta = Console.ReadLine();
 
             Console.WriteLine("Mora perto da vítima?");
-            Console.ReadLine();
+            string terceiraPergunta = Console.ReadLine();
 
             Console.WriteLine("Devia dinheiro para a vítima?");
-            Console.ReadLine();
+            string quartaPergunta = Console.ReadLine();
 
             Console.WriteLine("Já trabalhou com a vítima?");
-            Console.ReadLine();
+            string quintaPergunta = Console.ReadLine();
 
-            if (sim >= 2)
+            if (primeiraPergunta == "sim")
             {
-                Console.WriteLine("Você é suspeito pelo crime");
+                pontuacao++;
             }
-            else if (sim >= 3 && sim <= 4)
+            if (segundaPergunta == "sim")
             {
-                Console.WriteLine("Você foi cúmplice do crime");
+                pontuacao++;
             }
-            else if (sim == 5)
+            if (terceiraPergunta == "sim")
             {
-                Console.WriteLine("Você é o assassino!");
+                pontuacao++;
             }
-            else
+            if (quartaPergunta == "sim")
             {
-                Console.WriteLine("Você é inocente!");
+                pontuacao++;
             }
-
+            if (quintaPergunta == "sim")
+            {
+                pontuacao++;
+            }
+            
+            if (pontuacao == 2)
+            {
+                Console.WriteLine("Voce é suspeito");
+            }
+            else if (pontuacao >= 3 && pontuacao <= 4)
+            {
+                Console.WriteLine("Voce é cúmplice");
+            }
+            else if (pontuacao == 5)
+            {
+                Console.WriteLine("Voce é o assassino");
+            }
+            else if(pontuacao <2)
+            {
+                Console.WriteLine("Voce é inocente");
+            }
         }
     }
 }
